5ce25ad49398511af5fff0cb1e252d06d6ebbede
Gláucio Oliveira <glaucio@goliveira.com>
2020-02-06 20:03:01 -0300

    updating solution

--- check05a/main.cpp
+++ check05a/main.cpp
@@ -1,23 +1,31 @@
-/**********************************************************************
- * File: main.cpp
- * Author: Gláucio Oliveira
- *
- * Description: 
- *
- **********************************************************************/
+/*********************
+ * File: check05a.cpp
+ *********************/
+
+ // You should not change anything in this file 
+
 
 #include <iostream>
-
 using namespace std;
 
-/***********************************************************************
+#include "money.h"
+
+/****************************************************************
  * Function: main
- * Description: 
- ***********************************************************************/
+ * Purpose: Test the money class
+ ****************************************************************/
 int main()
 {
-    cout << "Hello world!\n";
-	return 0;
-}
+    Money money;
 
+    money.prompt();
+    money.display();
+
+    cout << endl;
+
+    cout << "That is " << money.getDollars() << " dollars and "
+        << money.getCents() << " cents!\n";
+
+    return 0;
+}
 
