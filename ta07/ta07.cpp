8b8500492d171a7835c3e64a59365baca08207b1
Gláucio Oliveira <glaucio@goliveira.com>
2020-02-20 18:59:07 -0300

    commiting changes automatically

--- ta07/ta07.cpp
+++ ta07/ta07.cpp
@@ -2,15 +2,52 @@
 using namespace std;
 
 // Core requirements
-float getValueFromPointer(float* thePointer);
-float* getMinValue(float* a, float* b);
+float getValueFromPointer(float* thePointer)
+{
+   return *thePointer;
+}
+
+float* getMinValue(float* a, float* b)
+{
+   if (getValueFromPointer(a) < getValueFromPointer(b))
+      return a;
+   else
+      return b;
+}
 
 // Stretch goals
 void swapElements(float* theArray[], int a, int b);
 void sortArray(float* theArray[]);
 
 int main()
-{  
+{
+   //Core Requirement 1
+   int arraySize;
+   cout << "Enter the array size: ";
+   cin >> arraySize;
+
+   //dynamic creation of the float array list;
+   float* arrayList = new float[arraySize];
+
+   //prompting the user for each value of the array list;
+   for (int i = 0; i < arraySize; i++)
+   {
+      cout << "Enter a float value: ";
+      cin >> arrayList[i];
+   }
+
+   //Core Requirement 2
+   for (int i = 0; i < arraySize; i++)
+   {
+      float value = getValueFromPointer(&arrayList[i]);
+      cout << "The value of the element " << i << " is: ";
+      cout << value << endl;
+   }
+
+   // Print the smaller of the first and last elements of the array
+   float* pointerToMin = getMinValue(&arrayList[0], &arrayList[arraySize - 1]);
+   cout << *pointerToMin;
+
    /* // Core Requirement 1
    int arraySize;
    cout << "Enter the array size: ";
@@ -19,7 +56,7 @@ int main()
    // Allocate your array(s) here
 
    // Fill your array with float values
-   for(int i = 0; i < arraySize; i++) 
+   for(int i = 0; i < arraySize; i++)
    {
           cout << "Enter a float value: ";
           cin >> ????
@@ -41,6 +78,7 @@ int main()
    */
 
    // Clean up your array(s) here
+   delete[] arrayList;
 
    return 0;
 }
