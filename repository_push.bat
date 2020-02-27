47e6757c5b07f8f187f703dda299c9779a9457f5
Glaucio Oliveira <glaucio.dev@gmail.com>
2020-02-27 18:00:26 -0300

    adding changes (automatically)

--- repository_push.bat
+++ repository_push.bat
@@ -1,4 +1,4 @@
-git config user.name "Gláucio Oliveira"
+git config user.name "Glaucio Oliveira"
 git config user.email "glaucio.dev@gmail.com"
 git add .
 git commit -m "adding changes (automatically)"
