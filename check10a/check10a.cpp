e4e8ba6e43b68a6074ce5d96b21117635b14f401
Glaucio Oliveira <glaucio.dev@gmail.com>
2020-03-10 16:29:17 -0300

    adding changes (automatically)

--- check10a/check10a.cpp
+++ check10a/check10a.cpp
@@ -1,25 +1,88 @@
 /***********************************************************************
 * Program:
 *    Checkpoint 10a, Vectors
-*    Brother {Burton, Falin, Ercanbrack}, CS165
+*    Brother Walker, CS165
 * Author:
-*    your name
-* Summary: 
+*    Glaucio Oliveira
+* Summary:
 *    Summaries are not necessary for checkpoint assignments.
 ************************************************************************/
-
-
-
 
 /**********************************************************************
  * Function: main
  * Purpose: This is the entry point and driver for the program.
  ***********************************************************************/
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 int main()
 {
+   bool leaveMenu = false;
+   vector <int> intList;
+   vector <string> stringList;
 
+   int intValue = 0;
+   string stringValue = "";
 
+   while (leaveMenu == false)
+   {
+      cout << "Enter int: ";
+      cin >> intValue;
 
+      if (cin.fail() == false)
+      {
+         if (intValue > 0)
+            intList.push_back(intValue);
+         else
+            leaveMenu = true;
+      }
+      else
+      {  
+         cin.ignore();
+         cin.clear('\n',256);         
+      }
+   }
+
+   cout << "Your list is:\n";
+
+   for (int i = 0; i < intList.size(); i++)
+   {
+      cout << intList[i] << endl;
+   }
+
+   // Processing Strings
+   leaveMenu = false;
+   cin.ignore();
+   cout << endl;
+
+   while (leaveMenu == false)
+   {
+      cout << "Enter string: ";
+      getline(cin, stringValue);
+
+      if (cin.fail() == false)
+      {
+         if (stringValue != "quit")
+            stringList.push_back(stringValue);
+         else
+            leaveMenu = true;
+      }
+      else
+      {
+         cin.clear('\n', 256);
+         cin.ignore();
+      }
+   }
+
+   cout << "Your list is:\n";
+
+   for (int i = 0; i < stringList.size(); i++)
+   {
+      cout << stringList[i] << endl;
+   }
    return 0;
 }
 
