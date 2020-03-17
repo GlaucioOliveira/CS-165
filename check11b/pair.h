aac9b461e831f72eecf6cf632be9e147ca8ce087
Glaucio Oliveira <glaucio.dev@gmail.com>
2020-03-17 11:57:39 -0300

    adding changes (automatically)

--- check11b/pair.h
+++ check11b/pair.h
@@ -8,8 +8,36 @@
 #ifndef PAIR_H
 #define PAIR_H
 
-// TODO: Fill in this class
-// Put all of your method bodies right in this file
+ // TODO: Fill in this class
+ // Put all of your method bodies right in this file
+template <class T1, class T2>
+class Pair
+{
+public:
+   void setFirst(T1 first) {
+      t1 = first;
+   };
 
+   void setSecond(T2 second) {
+      t2 = second;
+   };
+
+   T1 getFirst() const {
+      return t1;
+   };
+
+   T2 getSecond() const {
+      return t2;
+   };
+
+   void display() 
+   {
+      cout << t1 << " - " << t2 << endl;
+   }
+
+private:
+   T1 t1;
+   T2 t2;
+};
 
 #endif // PAIR_H
